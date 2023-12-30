05e467dd2f649c74078d0a24757159514c606d5d
Roman Olsen <romanolsen941@gmail.com>
2023-12-29 17:43:56 -0700

    Initial commit

--- ArrayString/Remove_Duplicates_from_Sorted_Array.cpp
+++ ArrayString/Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,5 +1,24 @@
-//Roman Olsen
-//Leetcode top 150
-// Remove Duplicates from Sorted Array
+// Roman Olsen
+// Leetcode top 150
+//  Remove Duplicates from Sorted Array
 //
-// To be completed
+#include <vector>
+
+class Solution {
+public:
+  int removeDuplicates(std::vector<int> &nums) {
+    if (nums.empty()) {
+      return 0;
+    }
+
+    int k = 1; // Initialize the count of unique elements to 1
+    for (int i = 1; i < nums.size(); i++) {
+      if (nums[i] != nums[i - 1]) {
+        nums[k] = nums[i]; // Overwrite the next unique element
+        k++;
+      }
+    }
+
+    return k;
+  }
+};
